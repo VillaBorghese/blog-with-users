60b1b3b6492ee91f1f2410784e2a23a09882b41b
Villa BORGHESE <rnjyomo@gmail.com>
2024-05-01 16:34:08 +0200

    Implemented python decorator @admin_only to limit access to create, edit and delete function on the blog

--- main.py
+++ main.py
@@ -35,6 +35,17 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 
+# admin decorator
+def admin_only(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not current_user.is_authenticated or current_user.id != 1:
+            return abort(403)
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
 # user loader
 @login_manager.user_loader
 def load_user(user_id):
@@ -159,7 +170,9 @@ def show_post(post_id):
 
 
 # TODO: Use a decorator so only an admin user can create a new post
+
 @app.route("/new-post", methods=["GET", "POST"])
+@admin_only
 def add_new_post():
     form = CreatePostForm()
     if form.validate_on_submit():
@@ -179,6 +192,7 @@ def add_new_post():
 
 # TODO: Use a decorator so only an admin user can edit a post
 @app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
+@admin_only
 def edit_post(post_id):
     post = db.get_or_404(BlogPost, post_id)
     edit_form = CreatePostForm(
@@ -201,6 +215,7 @@ def edit_post(post_id):
 
 # TODO: Use a decorator so only an admin user can delete a post
 @app.route("/delete/<int:post_id>")
+@admin_only
 def delete_post(post_id):
     post_to_delete = db.get_or_404(BlogPost, post_id)
     db.session.delete(post_to_delete)
